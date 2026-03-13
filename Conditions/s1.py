a95d6fee8bc8313f11d59a48399ecbc7fe774368
misbah254774-sketch <misbah254774@gmail.com>
2026-03-13 12:41:25 +0530

    Implement age classification in get_age function
    Fix age classification and input handling in get_age function.

---
+++ Conditions/s1.py
@@ -0,0 +1,25 @@
+# Online Python compiler (interpreter) to run Python online.
+def get_age():
+    while True:
+        try :
+            age = int(input("enter your age :--"))
+            if age <=0:
+                print("enter age in positive num")
+            elif age<13 :
+                 print("child")
+            elif age >=13 and age <19:
+                 print("Teen age")
+            elif age>=20 and age<59:
+                 print("adult")
+            elif age >=60 :
+                 print("senior")
+            elif :
+                breaK
+        except ValueError:
+            print("enter age in num")
+        
+        
+  
+  
+
+a = get_age()
