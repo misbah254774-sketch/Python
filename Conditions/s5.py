5c2100813b733095df2fc2862b12274416ed22b2
misbah254774-sketch <misbah254774@gmail.com>
2026-03-13 12:38:09 +0530

    Implement weather_condition function
    Added a function to respond to different weather conditions.

--- Conditions/s5.py
+++ Conditions/s5.py
@@ -1,1 +1,19 @@
-s
+def weather_condition():
+    while True:
+        try:
+            weather =str(input("enter weather condition :--")).lower()
+            if weather =="exit":
+                break
+            elif weather == "sunny":
+                print("go for induction program as there is ac installed ")
+            elif weather =="Rainy":
+                print("eat pakoda ")
+            elif weather == "snowy":
+                print("swim in a near by pond ")
+            else:
+                print("enter exact weather condition ")
+        except ValueError:
+            print("are you an idiot enter correct condition")
+            
+            
+a= weather_condition()
